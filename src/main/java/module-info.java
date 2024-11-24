fa3460679e85b733d89e36e1a7587e60c545d82d
Ruan Vasconcelos <ruanvasconcelos898@gmail.com>
2024-11-24 11:29:43 -0300

    add lombok depedency

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,8 @@ module com.trabfinal.finallpiii {
 
     requires com.dlsc.formsfx;
     requires org.kordamp.bootstrapfx.core;
+    requires jakarta.persistence;
+    requires static lombok;
 
     opens com.trabfinal.finallpiii to javafx.fxml;
     exports com.trabfinal.finallpiii;
