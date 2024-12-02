31526224d3652776a36a1cbb28ddd7a11566e3dd
Ruan Vasconcelos <ruanvasconcelos898@gmail.com>
2024-12-02 20:30:48 -0300

    created base.html

--- src/main/java/com/lpiii/trabFinal/Controllers/TeamController.java
+++ src/main/java/com/lpiii/trabFinal/Controllers/TeamController.java
@@ -4,12 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+import java.util.Map;
+
 @Controller
 public class TeamController {
     @GetMapping("/team")
     public String teamPage(Model model) {
-        model.addAttribute("title", "Página de Exemplo");
-        model.addAttribute("message", "Bem-vindo ao exemplo de Spring MVC com Thymeleaf!");
         return "pages/team";
     }
 }
