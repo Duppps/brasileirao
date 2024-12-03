ef4b51d2991df834c6b6e871d3dfdc6cc291f48f
Ruan Vasconcelos <ruanvasconcelos898@gmail.com>
2024-12-03 15:29:22 -0300

    add thymeleaf-layout-dialect to depedencies

--- src/main/java/com/lpiii/trabFinal/TrabFinalApplication.java
+++ src/main/java/com/lpiii/trabFinal/TrabFinalApplication.java
@@ -2,10 +2,13 @@ package com.lpiii.trabFinal;
 
 import com.lpiii.trabFinal.Entities.Team;
 import com.lpiii.trabFinal.Repositories.TeamRepository;
+import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.thymeleaf.spring6.SpringTemplateEngine;
 
 @SpringBootApplication
 public class TrabFinalApplication implements CommandLineRunner {
@@ -26,5 +29,10 @@ public class TrabFinalApplication implements CommandLineRunner {
 		teamRepository.save(gremio);
 
 		System.out.println("Team saved: " + gremio.getName());
+	}
+
+	@Bean
+	public LayoutDialect layoutDialect() {
+		return new LayoutDialect();
 	}
 }
