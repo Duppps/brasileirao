6f983a501da90fcb4d25cf463138579807ca156f
Ruan Vasconcelos <ruanvasconcelos898@gmail.com>
2024-12-09 21:41:04 -0300

    created edit in team crud

--- src/main/java/com/lpiii/trabFinal/Controllers/TeamController.java
+++ src/main/java/com/lpiii/trabFinal/Controllers/TeamController.java
@@ -7,26 +7,39 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 
 @Controller
+@RequestMapping("/teams")
 public class TeamController {
     @Autowired
     TeamRepository teamRepository;
 
-    @GetMapping("/team")
+    @GetMapping("/")
     public String getTeams(Model model) {
         List<Team> teamList = teamRepository.findAll();
         model.addAttribute("teamList", teamList);
         return "pages/team";
     }
 
-    @PostMapping("/team")
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updateTeam(@PathVariable Long id, @RequestBody Team teamUpdated) {
+        Team team = teamRepository.findById(id).orElseThrow(() -> new NoSuchElementException("Team not found with id " + id));
+
+        team.setName(teamUpdated.getName());
+        team.setCity(teamUpdated.getCity());
+        team.setStadium(teamUpdated.getStadium());
+
+        teamRepository.save(team);
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PostMapping("/")
     public ResponseEntity<String> createTeam(@RequestBody Team team) {
         teamRepository.save(team);
         return new ResponseEntity<>(HttpStatus.OK);
