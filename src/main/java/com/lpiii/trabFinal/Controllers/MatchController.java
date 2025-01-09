da39608f0851030dc9c5e1f5941caaac4b8cbad7
Ruan Vasconcelos <ruanvasconcelos898@gmail.com>
2025-01-09 17:41:06 -0300

    update match now update the teams points

--- src/main/java/com/lpiii/trabFinal/Controllers/MatchController.java
+++ src/main/java/com/lpiii/trabFinal/Controllers/MatchController.java
@@ -10,11 +10,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.Timestamp;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/matches")
@@ -39,20 +42,86 @@ public class MatchController {
         return "pages/match";
     }
 
+    @Transactional
     @PutMapping("/{id}")
-    public ResponseEntity<String> updateMatch(@PathVariable Long id, @RequestBody Match matchUpdated) {
-        Match match = matchRepository.findById(id).orElseThrow(() -> new NoSuchElementException("Match not found with id " + id));
+    public ResponseEntity<String> updateMatch(@PathVariable Long id, @RequestBody MatchDTO matchDTO) {
+        Match match = matchRepository.findById(id)
+                .orElseThrow(() -> new NoSuchElementException("Match not found with id " + id));
 
-        match.setHomeTeam(matchUpdated.getHomeTeam());
-        match.setVisitingTeam(matchUpdated.getVisitingTeam());
-        match.setGoalsHomeTeam(matchUpdated.getGoalsHomeTeam());
-        match.setGoalsVisitingTeam(matchUpdated.getGoalsVisitingTeam());
-        match.setDateTime(matchUpdated.getDateTime());
+        Team homeTeam = match.getHomeTeam();
+        Team visitingTeam = match.getVisitingTeam();
+
+        if (match.getGoalsHomeTeam() > match.getGoalsVisitingTeam()) {
+            homeTeam.setPoints(homeTeam.getPoints() - 3);
+            homeTeam.setWins(homeTeam.getWins() - 1);
+            visitingTeam.setLosses(visitingTeam.getLosses() - 1);
+        } else if (match.getGoalsHomeTeam() < match.getGoalsVisitingTeam()) {
+            visitingTeam.setPoints(visitingTeam.getPoints() - 3);
+            visitingTeam.setWins(visitingTeam.getWins() - 1);
+            homeTeam.setLosses(homeTeam.getLosses() - 1);
+        } else {
+            homeTeam.setPoints(homeTeam.getPoints() - 1);
+            visitingTeam.setPoints(visitingTeam.getPoints() - 1);
+            homeTeam.setDraws(homeTeam.getDraws() - 1);
+            visitingTeam.setDraws(visitingTeam.getDraws() - 1);
+        }
+
+        homeTeam.setGoalsScored(homeTeam.getGoalsScored() - match.getGoalsHomeTeam());
+        homeTeam.setGoalsConceded(homeTeam.getGoalsConceded() - match.getGoalsVisitingTeam());
+        visitingTeam.setGoalsScored(visitingTeam.getGoalsScored() - match.getGoalsVisitingTeam());
+        visitingTeam.setGoalsConceded(visitingTeam.getGoalsConceded() - match.getGoalsHomeTeam());
+
+        Optional<Team> optionalHomeTeam = teamRepository.findById(matchDTO.homeTeamId());
+        Optional<Team> optionalVisitingTeam = teamRepository.findById(matchDTO.visitingTeamId());
+
+        if (optionalHomeTeam.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Home team not found");
+        }
+        if (optionalVisitingTeam.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Visiting team not found");
+        }
+
+        Team updatedHomeTeam = optionalHomeTeam.get();
+        Team updatedVisitingTeam = optionalVisitingTeam.get();
+
+        int goalsHomeTeam = matchDTO.homeGoals();
+        int goalsVisitingTeam = matchDTO.visitingGoals();
+        Timestamp dateTime = matchDTO.dateTime();
+
+        match.setHomeTeam(updatedHomeTeam);
+        match.setVisitingTeam(updatedVisitingTeam);
+        match.setGoalsHomeTeam(goalsHomeTeam);
+        match.setGoalsVisitingTeam(goalsVisitingTeam);
+        match.setDateTime(dateTime);
+
+        if (goalsHomeTeam > goalsVisitingTeam) {
+            updatedHomeTeam.addPoints(3);
+            updatedHomeTeam.addWin();
+            updatedVisitingTeam.addLoss();
+        } else if (goalsHomeTeam < goalsVisitingTeam) {
+            updatedVisitingTeam.addPoints(3);
+            updatedVisitingTeam.addWin();
+            updatedHomeTeam.addLoss();
+        } else {
+            updatedHomeTeam.addPoints(1);
+            updatedVisitingTeam.addPoints(1);
+            updatedHomeTeam.addDraw();
+            updatedVisitingTeam.addDraw();
+        }
+
+        updatedHomeTeam.addGoalsScored(goalsHomeTeam);
+        updatedHomeTeam.addGoalsConceded(goalsVisitingTeam);
+        updatedVisitingTeam.addGoalsScored(goalsVisitingTeam);
+        updatedVisitingTeam.addGoalsConceded(goalsHomeTeam);
 
         matchRepository.save(match);
+        teamRepository.save(updatedHomeTeam);
+        teamRepository.save(updatedVisitingTeam);
 
         return new ResponseEntity<>(HttpStatus.OK);
     }
+
+
 
     @PostMapping("/")
     public ResponseEntity<String> createMatch(@RequestBody MatchDTO matchDTO) {
@@ -71,10 +140,38 @@ public class MatchController {
 
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteMatch(@PathVariable Long id) {
-        Match match = matchRepository.findById(id).orElseThrow(() -> new NoSuchElementException("Match not found with id " + id));
+        Match match = matchRepository.findById(id)
+                .orElseThrow(() -> new NoSuchElementException("Match not found with id " + id));
+
+        Team homeTeam = match.getHomeTeam();
+        Team visitingTeam = match.getVisitingTeam();
+
+        if (match.getGoalsHomeTeam() > match.getGoalsVisitingTeam()) {
+            homeTeam.setPoints(homeTeam.getPoints() - 3);
+            homeTeam.setWins(homeTeam.getWins() - 1);
+            visitingTeam.setLosses(visitingTeam.getLosses() - 1);
+        } else if (match.getGoalsHomeTeam() < match.getGoalsVisitingTeam()) {
+            visitingTeam.setPoints(visitingTeam.getPoints() - 3);
+            visitingTeam.setWins(visitingTeam.getWins() - 1);
+            homeTeam.setLosses(homeTeam.getLosses() - 1);
+        } else {
+            homeTeam.setPoints(homeTeam.getPoints() - 1);
+            visitingTeam.setPoints(visitingTeam.getPoints() - 1);
+            homeTeam.setDraws(homeTeam.getDraws() - 1);
+            visitingTeam.setDraws(visitingTeam.getDraws() - 1);
+        }
+
+        homeTeam.setGoalsScored(homeTeam.getGoalsScored() - match.getGoalsHomeTeam());
+        homeTeam.setGoalsConceded(homeTeam.getGoalsConceded() - match.getGoalsVisitingTeam());
+        visitingTeam.setGoalsScored(visitingTeam.getGoalsScored() - match.getGoalsVisitingTeam());
+        visitingTeam.setGoalsConceded(visitingTeam.getGoalsConceded() - match.getGoalsHomeTeam());
+
+        teamRepository.save(homeTeam);
+        teamRepository.save(visitingTeam);
 
         matchRepository.delete(match);
 
         return new ResponseEntity<>(HttpStatus.OK);
     }
+
 }
